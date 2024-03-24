dee7c7c5b6cf0265e633b3f09e5282f8233d687c
HassanHassouna <hhasona@gmail.com>
2024-03-24 15:50:12 +0200

    backend instance

--- apps/client/src/app/api/api.ts
+++ apps/client/src/app/api/api.ts
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 const version = 'v1';
 export const backendInstance = axios.create({
-  baseURL: `http://localhost:8000/api/${version}`
+  baseURL: `https://e-learning-back-ufeg.onrender.com/api/${version}`
 });
